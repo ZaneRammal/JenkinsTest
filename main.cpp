72c7a9067f0423c1470c149e4942067f4f8919a8
Zane Rammal <49923551+ZaneRammal@users.noreply.github.com>
2019-10-19 12:55:48 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      :
+// Version     :
+// Copyright   :
+// Description : Hello World in C++
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	return 0;
+}
